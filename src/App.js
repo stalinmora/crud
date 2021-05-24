318a40ca685ce3d1d5c8c800076a7f8b09bc1254
stalinmora <stalin.mora.burgos@gmail.com>
2021-05-23 22:12:15 -0500

    23/05/2021
    Primer Ommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Tareas</h1>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
